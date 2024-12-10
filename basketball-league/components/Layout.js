80a86482c9c96913d9f906c608cdd1e8a85429c6
Patrick <patriciopalaciosrivera@gmail.com>
2024-12-09 19:55:06 -0600

    ✨Dar estilos a mi componente Layout

--- basketball-league/components/Layout.js
+++ basketball-league/components/Layout.js
@@ -1,7 +1,67 @@
-import React from 'react'
+import Head from "next/head"
+import{
+    Box,
+    Flex,
+    Text,
+    Stack,
+    Button,
+    useColorModeValue
+} from "@chakra-ui/react"
+
 const Layout = () => {
   return (
-    <div>Layout</div>
+    <div>
+      <Head>
+        <title>Basquetball League</title>
+      </Head>
+            
+      <Box>
+      {/* Box es como un Div en Chakra-UI */}   
+        <Flex        
+      bg={useColorModeValue('gray.300', 'gray.600')}
+      minH={'60px'}
+      py={{base: 2}}
+      px={{base: 2}}
+      borderTop={1}
+      borderBottom={1}
+      borderStartStyle={'solid'}
+      borderColor={useColorModeValue('gray.200', 'gray.900')}
+      align={'center'}
+        >
+          <Flex
+          flex={{base: 1}}
+          justify={{base: 'center', md: 'start'}}          
+          >
+            <Text
+            fontFamily={'heading'}
+            color={useColorModeValue('gray.800', 'white')}              
+            >Logo</Text>
+          </Flex>
+          <Stack
+          flex={{base: 1}}
+          justify={'flex-end'}
+          direction={'row'}
+          spacing={6}
+          >
+            <Button
+            fontSize={'sm'}
+            fontWeight={400}
+            variant={'link'}
+            href={'#'}
+            >Sign in</Button>
+            <Button
+            fontSize={'md'}
+            fontWeight={600}
+            color={'white'}
+            variant={'link'}
+            href={'#'}
+            bg={'blue.400'}
+            _hover={{ bg: 'blue.300'}}
+            >Sign up</Button>            
+          </Stack>
+        </Flex>
+      </Box>        
+    </div>
   )
 }
 export default Layout
