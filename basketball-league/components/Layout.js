0417adf562209b82ba8539c2d8de8e6fc2e9c3f0
Patrick <patriciopalaciosrivera@gmail.com>
2024-12-10 00:36:29 -0600

    ✨Actualización de mi componente Layout

--- basketball-league/components/Layout.js
+++ basketball-league/components/Layout.js
@@ -1,67 +1,72 @@
-import Head from "next/head"
-import{
-    Box,
-    Flex,
-    Text,
-    Stack,
-    Button,
-    useColorModeValue
-} from "@chakra-ui/react"
+import Head from "next/head";
+import Image from "next/image"; // Importación del componente Image
+import { Box, Flex, Text, Stack, Button, useColorModeValue } from "@chakra-ui/react";
+import { useRouter } from "next/router"; // Importación de useRouter
 
-const Layout = () => {
+const Layout = ({ children }) => {
+  const router = useRouter(); // Inicializamos el router para manejar la navegación
+
   return (
     <div>
       <Head>
         <title>Basquetball League</title>
       </Head>
-            
+
       <Box>
-      {/* Box es como un Div en Chakra-UI */}   
-        <Flex        
-      bg={useColorModeValue('gray.300', 'gray.600')}
-      minH={'60px'}
-      py={{base: 2}}
-      px={{base: 2}}
-      borderTop={1}
-      borderBottom={1}
-      borderStartStyle={'solid'}
-      borderColor={useColorModeValue('gray.200', 'gray.900')}
-      align={'center'}
+        <Flex
+          bg={useColorModeValue("gray.300", "gray.600")}
+          minH={"60px"}
+          py={{ base: 2 }}
+          px={{ base: 2 }}
+          borderTop={1}
+          borderBottom={1}
+          borderStartStyle={"solid"}
+          borderColor={useColorModeValue("gray.200", "gray.900")}
+          align={"center"}
         >
-          <Flex
-          flex={{base: 1}}
-          justify={{base: 'center', md: 'start'}}          
-          >
-            <Text
-            fontFamily={'heading'}
-            color={useColorModeValue('gray.800', 'white')}              
-            >Logo</Text>
+          <Flex flex={{ base: 1 }} justify={{ base: "center", md: "start" }}>
+            <Text fontFamily={"heading"} color={useColorModeValue("gray.800", "white")}>
+              {/* Imagen del logotipo */}
+              <Image
+                src="/logo.jpg" // Ruta de la imagen en la carpeta public
+                alt="Logo de la Liga de Básquetbol"
+                width={80} // Ajusta tamaño
+                height={80}
+              />
+            </Text>
           </Flex>
-          <Stack
-          flex={{base: 1}}
-          justify={'flex-end'}
-          direction={'row'}
-          spacing={6}
-          >
+          <Stack flex={{ base: 1 }} justify={"flex-end"} direction={"row"} spacing={6}>
+            {/* Botón Sign in redirige a /login */}
             <Button
-            fontSize={'sm'}
-            fontWeight={400}
-            variant={'link'}
-            href={'#'}
-            >Sign in</Button>
+              fontSize={"md"}
+              fontWeight={800}
+              color={"gray.600"}
+              variant={"link"}
+              bg={"blue.500"}
+              _hover={{ bg: "blue.300" }}
+              onClick={() => router.push("/login")} // Redirige al login
+            >
+              Sign in
+            </Button>
+            {/* Botón Sign up redirige a /register */}
             <Button
-            fontSize={'md'}
-            fontWeight={600}
-            color={'white'}
-            variant={'link'}
-            href={'#'}
-            bg={'blue.400'}
-            _hover={{ bg: 'blue.300'}}
-            >Sign up</Button>            
+              fontSize={"md"}
+              fontWeight={800}
+              color={"gray.600"}
+              variant={"link"}
+              bg={"blue.400"}
+              _hover={{ bg: "blue.300" }}
+              onClick={() => router.push("/register")} // Redirige al registro
+            >
+              Sign up
+            </Button>
           </Stack>
         </Flex>
-      </Box>        
+      </Box>
+
+      <Box>{children}</Box>
     </div>
-  )
-}
-export default Layout
+  );
+};
+
+export default Layout;
