33be27d8d056b48dc09072139f7f2c0bb66be662
Patrick <patriciopalaciosrivera@gmail.com>
2024-12-10 23:11:14 -0600

    ✨Actualización de mi index.js al agregar imagen de background.jpg

--- basketball-league/pages/index.js
+++ basketball-league/pages/index.js
@@ -1,12 +1,26 @@
-import Layout from "../components/Layout"
+import Layout from "../components/Layout";
+import { Box } from "@chakra-ui/react";
 
 export default function Home() {
   return (
-    <>
+    <Box
+      w="100%"
+      h="100vh"
+      bgImage="url('/background.jpg')" // Ruta de la imagen en la carpeta public
+      bgSize="cover" // Asegura que la imagen cubra todo el área
+      bgPosition="center" // Centra la imagen
+      bgRepeat="no-repeat" // No repite la imagen
+    >
       <Layout>
-        
+        {/* Aquí puedes añadir contenido adicional */}
       </Layout>
-      <h2>FUNCIONANDO</h2>
-    </>
-  )
+      <h2 style={{ color: "white",
+         textAlign: "center",
+         marginTop: "30px",
+         fontSize: "24px", 
+         }}>
+      "Tu momento de brillar empieza aquí."
+      </h2>
+    </Box>
+  );
 }
